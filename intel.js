d2a02a80daf4f86dbe770109adf5e8a3d41c4283
Unknown <wfuentes1@mail.csuchico.edu>
2017-03-30 11:56:48 -0700

    work on java

--- intel.js
+++ intel.js
@@ -1,4 +1,15 @@
+var answerEntered;
 function validate(){
-  var answerEntered = document.getElementById("answer1").value;
-  if (answerEntered >= )
+ answerEntered = document.getElementById("answer1").value;
+  if (answerEntered == 2565 ){
+  document.getElementById("answer1").id.add("go job");
+}else {
+  document.getElementById("answer1").id.add("wrong answer");
+}
+answerEntered = document.getElementById("answer2").value;
+
+answerEntered = document.getElementById("answer3").value;
+answerEntered = document.getElementById("answer4").value;
+answerEntered = document.getElementById("answer5").value;
+answerEntered = document.getElementById("answer6").value;
 }
