55618fbe373aa506a5d2ef0d881196b3f600d48c
Unknown <wfuentes1@mail.csuchico.edu>
2017-03-30 12:15:03 -0700

    java

--- intel.js
+++ intel.js
@@ -1,15 +1,44 @@
 var answerEntered;
+var answerEntered2;
+var answerEntered3;
+var answerEntered4;
+var answerEntered5;
+var answerEntered6;
 function validate(){
  answerEntered = document.getElementById("answer1").value;
   if (answerEntered == 2565 ){
-  document.getElementById("answer1").id.add("go job");
+  document.getElementById("answer1").innerHTML ="go job";
 }else {
-  document.getElementById("answer1").id.add("wrong answer");
+  document.getElementById("answer1").innerHTML= "wrong answer";
 }
 answerEntered = document.getElementById("answer2").value;
-
+if(answerEntered2 == 54146 ){
+  document.getElementById("answer2").innerHTML= "go job";
+}else {
+  document.getElementById("answer2").innerHTM= "wrong answer";
+}
 answerEntered = document.getElementById("answer3").value;
+if(answerEntered3 ==205 ){
+  document.getElementById("answer3").innerHTML ="go job";
+}else {
+  document.getElementById("answer3").innerHTML="wrong answer";
+}
 answerEntered = document.getElementById("answer4").value;
+if(answerEntered4== 9010){
+  document.getElementById("answer4").innerHTML="go job";
+}else {
+  document.getElementById("answer4").innerHTML= "wrong answer";
+}
 answerEntered = document.getElementById("answer5").value;
+if(answerEntered5== 822105){
+  document.getElementById("answer5").innerHTML= "go job";
+}else {
+  document.getElementById("answer5").id.add("wrong answer");
+}
 answerEntered = document.getElementById("answer6").value;
+if answerEntered6== 3246273<){
+  document.getElementById("answer6").innerHTML= "go job";
+}else {
+  document.getElementById("answer6").innerHTml= "wrong answer";
+}
 }
